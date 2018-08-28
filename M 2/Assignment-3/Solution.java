e006b6da193978c538cd1c9918aaf339652a88dc
ASairithwikmahateja <avschalapathirao@msitprogram.net>
2018-08-28 14:50:24 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- M 2/Assignment-3/Solution.java
+++ M 2/Assignment-3/Solution.java
@@ -5,7 +5,7 @@ public class Solution {
 	/*
 	Do not modify this main function.
 	*/
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		Scanner s = new Scanner(System.in);      
         int base = s.nextInt();
         int exponent = s.nextInt();
@@ -15,15 +15,18 @@ public class Solution {
 	/*
 	Need to write the power function and print the output.
 	*/
-	static long power(int base, int exponent) {
+	static long power(final int base, final int exponent) {
 		if (exponent == 0) {
 			int pow = 1;
-			return pow; }
+			return pow;
+			 }
 		else if (exponent == 1) {
 			int pow = base;
-			return pow; }
+			return pow;
+			 }
 		else {
 			long pow = base * power(base, exponent - 1);
-			return pow; }
+			return pow;
+			 }
 	}
 }
