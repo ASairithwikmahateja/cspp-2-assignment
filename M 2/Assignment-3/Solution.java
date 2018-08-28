f7ac4ca53b1555bc8f8cc6778d6e37e2395e0736
ASairithwikmahateja <avschalapathirao@msitprogram.net>
2018-08-28 18:09:27 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- M 2/Assignment-3/Solution.java
+++ M 2/Assignment-3/Solution.java
@@ -1,30 +1,42 @@
 import java.util.Scanner;
-import java.lang.Math.*;
-
-public class Solution {
-	/*
-	Do not modify this main function.
-	*/
-	public static void main(final String[] args) {
-		Scanner s = new Scanner(System.in);
-		int base = s.nextInt();
+//import java.lang.Math.*;
+/**
+@author Mahateja
+*/
+public final class Solution {
+    /*
+    Do not modify this main function.
+    */
+    /**
+    @default constructor
+    */
+    private Solution() { }
+    /**
+    @param  args string
+    */
+    public static void main(final String[] args) {
+        Scanner s = new Scanner(System.in);
+        int base = s.nextInt();
         int exponent = s.nextInt();
         long result = power(base, exponent);
         System.out.println(result);
-	}
-	/*
-	Need to write the power function and print the output.
-	*/
-	static long power(final int base, final int exponent) {
-		if (exponent == 0) {
-			int pow = 1;
-			return pow;
-			 } else if (exponent == 1) {
-				int pow = base;
-				return pow;
-			 } else {
-				long pow = base * power(base, exponent - 1);
-				return pow;
-			 }
-	}
+    }
+    /**
+    Need to write the power function and print the output.
+    @param base int
+    @param exponent int
+    @return pow
+    */
+    static long power(final int base, final int exponent) {
+        if (exponent == 0) {
+            int pow = 1;
+            return pow;
+             } else if (exponent == 1) {
+                int pow = base;
+                return pow;
+             } else {
+                long pow = base * power(base, exponent - 1);
+                return pow;
+             }
+    }
 }
