8ab7d0fd58ccb4f752787ceec014f9ed485ea544
ASairithwikmahateja <avschalapathirao@msitprogram.net>
2018-08-29 18:54:11 +0530

    new

---
+++ M 3/ConverttoWhileLoop/ConverttoWhileLoop.java
@@ -0,0 +1,25 @@
+//import java.util.*;
+/**
+@author Mahateja
+*/
+public final class ConverttoWhileLoop {
+    /**
+    Do not modify this class
+    */
+    /**
+    @default constructor
+    */
+    private ConverttoWhileLoop() { }
+    /**
+    @param  args string
+    */
+    public static void main(final String[] args) {
+        int i = 2;
+        final int br = 10;
+        while (i <= br) {
+            System.out.println(i);
+            i = i + 2;
+        }
+        System.out.println("GoodBye!");
+    }
+}
