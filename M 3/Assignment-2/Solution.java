b7ccff3b96c566f77d7d649665d8d1a8232123a1
ASairithwikmahateja <avschalapathirao@msitprogram.net>
2018-08-29 17:27:45 +0530

    "check_style_score -> 0 of 5 passed. style score: 0/1 "

--- M 3/Assignment-2/Solution.java
+++ M 3/Assignment-2/Solution.java
@@ -20,11 +20,13 @@ public final class Solution {
         Scanner s = new Scanner(System.in);      
         int n = s.nextInt();
         int i = n, cnt = 0, j = 0;
-        for (;i >= 0; i--) {
+        for (i = n; i >= 0; i--) {
         	j = i % 10;
-        	for (;j == 7; j--) {
+        	if (j == 7) {
+   	    	    j = i % 10;
    	    	    cnt = cnt + 1;
-       	    } i = i /10;
+       	        i = i / 10;
+       	    }
        	} System.out.println(cnt);
    	}
 } 
