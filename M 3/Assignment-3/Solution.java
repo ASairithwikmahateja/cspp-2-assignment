1747c8849e349fb9c8b38913d8faa0466e379d07
ASairithwikmahateja <avschalapathirao@msitprogram.net>
2018-08-29 13:57:45 +0530

    "check_style_score -> 1 of 5 passed. style score: 0/1 "

--- M 3/Assignment-3/Solution.java
+++ M 3/Assignment-3/Solution.java
@@ -12,7 +12,6 @@ public class Solution {
         int n1 = s.nextInt();
         int n2 = s.nextInt();
         gcd(n1, n2);
-        System.out.println(gcd(n1, n2));
     }
     /*
 	Need to write the gcd function and print the output.
@@ -21,25 +20,25 @@ public class Solution {
      * 
      *
      * @param      n1    int
-     * @param      n2    int
-     *
-     * @return     gcd   int 
+     * @param      n2    int 
      */
-    static int gcd(int n1, int n2) {
+    static void gcd(int n1, int n2) {
         int gcd = 0;
         int i = 2;
         int tmp = 0;
         if (n1 > n2) {
             for (i = 2; i <= n1; i++) {
-            if (n1 % i == 0 & n2 % i == 0) {
-                gcd = i;
-            } return gcd;
+                if (n1 % i == 0 & n2 % i == 0) {
+                    gcd = i;
+                } 
+                System.out.println(gcd);
             }                        
         } else if (n1 < n2) {
             tmp = n1;
             n1 = n2;
             n2 = tmp;
             gcd(n1, n2);
-        } return 1;
+        } else if (n1 == n2)
+            System.out.println(n1);
     }
 }
