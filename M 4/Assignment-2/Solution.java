bfc9455f81369abc203f96fd7e786f11e829f8c6
ASairithwikmahateja <avschalapathirao@msitprogram.net>
2018-08-30 18:57:53 +0530

    "check_style_score -> 2 of 4 passed. style score: 0/1 "

--- M 4/Assignment-2/Solution.java
+++ M 4/Assignment-2/Solution.java
@@ -39,7 +39,7 @@ public final class Solution {
             int[][] c = new int[row][col];
             for (int i = 0; i < row; i++) {
                 for (int j = 0; j < col; j++) {
-                	if (i != row & j != col) {
+                    if (i != row & j != col) {
                         c[i][j] = a[i][j] + b[i][j];
                     System.out.print(c[i][j] + " ");
                 	}
