61b491188daffe1db94f2772ad81564942ff62f1
ASairithwikmahateja <avschalapathirao@msitprogram.net>
2018-08-30 15:38:10 +0530

    "check_style_score -> 2 of 4 passed. style score: 0/1 "

--- M 4/Assignment-2/Solution.java
+++ M 4/Assignment-2/Solution.java
@@ -40,7 +40,7 @@ public final class Solution {
             for (int i = 0; i < row; i++) {
                 for (int j = 0; j < col; j++) {
                     c[i][j] = a[i][j] + b[i][j];
-        	        System.out.print(c[i][j] + " ");
+                    System.out.print(c[i][j] + " ");
         	    } System.out.println();
             }
         } else {
