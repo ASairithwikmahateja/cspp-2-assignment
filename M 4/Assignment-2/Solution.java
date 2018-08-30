1c46a8f226b3f16f3cd3d5125f626f44a629ee71
ASairithwikmahateja <avschalapathirao@msitprogram.net>
2018-08-30 14:12:36 +0530

    "check_style_score -> 2 of 4 passed. style score: 0/1 "

--- M 4/Assignment-2/Solution.java
+++ M 4/Assignment-2/Solution.java
@@ -25,7 +25,7 @@ public final class Solution {
             for (int j = 0; j < col; j++) {
                 a[i][j] = sc.nextInt();
         	    //System.out.println(a[i][j]);
-        	} 
+        	}
         }
         int row1 = sc.nextInt();
         int col1 = sc.nextInt();
@@ -34,7 +34,7 @@ public final class Solution {
             for (int j = 0; j < col1; j++) {
                 b[i][j] = sc.nextInt();
         	    //System.out.println(b[i][j]);
-        	} 
+        	}
         }
         if (row == row1 & col == col1) {
         	int[][] c = new int[row][col];
