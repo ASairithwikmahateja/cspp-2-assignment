ee3637ab73239987f6723d8bcb1eda072f28e52f
ASairithwikmahateja <avschalapathirao@msitprogram.net>
2018-08-30 13:48:21 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ M 4/Assignment-2/Solution.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+    /*
+    Do not modify this main function.
+    */
+    /**
+    @author Mahateja
+    */
+public final class Solution {
+	/* Fill the main function to print resultant of addition of matrices*/
+    /**
+    @default constructor
+    */
+    private Solution() { }
+    /**
+     *
+     * @param      args    String
+     */
+	public static void main(final String[] args) {
+		Scanner sc = new Scanner(System.in);
+        int row = sc.nextInt();
+        int col = sc.nextInt();
+        int[][] a = new int[row][col];
+        int[][] b = new int[row][col];
+        int[][] c = new int[row][col];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                a[i][j] = sc.nextInt();
+        	    System.out.println(a[i][j]);
+        	} 
+        }
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                b[i][j] = sc.nextInt();
+        	    System.out.println(b[i][j]);
+        	} 
+        }
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                c[i][j] = a[i][j] + b[i][j];
+        	    System.out.println(c[i][j]);
+        	} 
+        }
+	}
+}
