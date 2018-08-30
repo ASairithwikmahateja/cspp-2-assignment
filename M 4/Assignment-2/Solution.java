7e777a8a9293e26a4d99ff5ec66106a0606b99a6
ASairithwikmahateja <avschalapathirao@msitprogram.net>
2018-08-30 14:00:34 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- M 4/Assignment-2/Solution.java
+++ M 4/Assignment-2/Solution.java
@@ -20,25 +20,29 @@ public final class Solution {
         int row = sc.nextInt();
         int col = sc.nextInt();
         int[][] a = new int[row][col];
-        int[][] b = new int[row][col];
-        int[][] c = new int[row][col];
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
                 a[i][j] = sc.nextInt();
         	    //System.out.println(a[i][j]);
         	} 
         }
+        int row1 = sc.nextInt();
+        int col1 = sc.nextInt();
+        int[][] b = new int[row1][col1];
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
                 b[i][j] = sc.nextInt();
         	    //System.out.println(b[i][j]);
         	} 
         }
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                c[i][j] = a[i][j] + b[i][j];
-        	    System.out.println(c[i]);
-        	} 
+        if (row == row1 & col == col1) {
+        	int[][] c = new int[row][col];
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; j < col; j++) {
+                    c[i][j] = a[i][j] + b[i][j];
+        	        System.out.print(c[i][j]);
+        	    } System.out.println();
+            }
         }
 	}
 }
