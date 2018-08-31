277ae27be1bcacbe7cbbf458a481cf79a4eeab04
ASairithwikmahateja <avschalapathirao@msitprogram.net>
2018-08-31 17:00:40 +0530

    "check_style_score -> 5 of 5 passed. style score: 1/1 "

--- M 3/Assignment-3/Solution.java
+++ M 3/Assignment-3/Solution.java
@@ -31,20 +31,18 @@ public final class Solution {
      */
     static void gcd(final int n1, final int n2) {
         int gcd = 0;
-        int i = 2;
-        int tmp = 0;
-        int a = n1, b = n2;
+        int swptmp = 0, num1 = n1, num2 = n2;
         if (n1 > n2) {
-            for (i = 2; i <= n1; i++) {
+            for (int i = 2; i <= n1; i++) {
                 if (n1 % i == 0 & n2 % i == 0) {
                     gcd = i;
                 }
             } System.out.println(gcd);
         } else if (n1 < n2) {
-            tmp = a;
-            a = b;
-            b = tmp;
-            gcd(a, b);
+            swptmp = num1;
+            num1 = num2;
+            num2 = swptmp;
+            gcd(num1, num2);
         } else if (n1 == n2) {
             System.out.println(n1);
         }
