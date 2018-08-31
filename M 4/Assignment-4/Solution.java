b77f23fde3c7608bed0e5ea69fe0a4429a5a2357
ASairithwikmahateja <avschalapathirao@msitprogram.net>
2018-08-31 17:15:03 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- M 4/Assignment-4/Solution.java
+++ M 4/Assignment-4/Solution.java
@@ -1,13 +1,14 @@
-// Given an String, Write a java method that returns the decimal value for the given binary string.
+/* Given an String, Write a java method that returns 
+the decimal value for the given binary string.*/
 import java.util.Scanner;
     /**
     @author Mahateja
     */
-public class Solution {
+public final class Solution {
     /*
-	Do not modify the main function 
-	*/
-	/**
+    Do not modify the main function
+    */
+    /**
     @default constructor
     */
     private Solution() { }
@@ -15,20 +16,20 @@ public class Solution {
      *
      * @param      args    String
      */
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String s = sc.nextLine();
-		String reverse = reverseString(s);	
-		System.out.println(reverse);
-	}
-	//Write reverseString function
-	/**
-	 *
-	 * @param      s     String
-	 *
-	 * @return     rev
-	 */
-	static String reverseString(final String s) {
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String reverse = reverseString(s);
+        System.out.println(reverse);
+    }
+    //Write reverseString function
+    /**
+     *
+     * @param      s     String
+     *
+     * @return     rev
+     */
+    static String reverseString(final String s) {
         String revstr = "";
         for (int i = 0; i < s.length(); i++) {
         	revstr = s.charAt(i) + revstr;
