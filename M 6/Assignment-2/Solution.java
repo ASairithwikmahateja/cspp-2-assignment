e9eee9df7c5a1d162241f4a87a3c974b7b7ba9f4
ASairithwikmahateja <avschalapathirao@msitprogram.net>
2018-09-01 10:53:49 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- M 6/Assignment-2/Solution.java
+++ M 6/Assignment-2/Solution.java
@@ -24,27 +24,28 @@ final class Solution {
      * @return     Matrix of the rounded elements
      */
     static int[][] roundHundred(final int[][] a, final int rows, final int columns) {
-
 	// write your code here
         int m = rows; 
         int n = columns;
         int[][] b = new int[m][n];
         final int zer = 0, fift = 50, hunft = 150,
-        tohunf = 250, trhunf = 350, fohunf = 450, fihunf = 550;   
+        tohunf = 250, trhunf = 350, fohunf = 450, fihunf = 550;
+        final int hundred = 100, tohundred = 200,
+        trhundred = 300, fohundred = 400, fihundred = 500;  
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (a[i][j] >= zer & a[i][j] < fift) {
-                    b[i][j] = 0;
+                    b[i][j] = zer;
                 } else if (a[i][j] >= fift & a[i][j] < hunft) {
-                    b[i][j] = 100;
+                    b[i][j] = hundred;
                 } else if (a[i][j] >= hunft & a[i][j] < tohunf) {
-                    b[i][j] = 200;
+                    b[i][j] = tohundred;
                 } else if (a[i][j] >= tohunf & a[i][j] < trhunf) {
-                    b[i][j] = 300;
+                    b[i][j] = trhundred;
                 } else if (a[i][j] >= trhunf & a[i][j] < fohunf) {
-                    b[i][j] = 400;
+                    b[i][j] = fohundred;
                 } else if (a[i][j] >= fohunf & a[i][j] < fihunf) {
-                    b[i][j] = 500;
+                    b[i][j] = fihundred;
                 }
             }
         } return b;
