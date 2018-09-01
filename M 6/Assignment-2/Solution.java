e9b1122ba65881a1d45e2015d705fe2f11cd9aa5
ASairithwikmahateja <avschalapathirao@msitprogram.net>
2018-09-01 10:44:29 +0530

    "check_style_score -> 0 of 5 passed. style score: 0/1 "

--- M 6/Assignment-2/Solution.java
+++ M 6/Assignment-2/Solution.java
@@ -31,7 +31,7 @@ final class Solution {
         int[][] b = new int[m][n]; 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                System.out.println(a[i][j]);
+                b[i][j] = a[i][j];
             }
         } return b;
 	
