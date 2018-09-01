76a1829793a22b545617cbf08a5a0c25c256e3ad
ASairithwikmahateja <avschalapathirao@msitprogram.net>
2018-09-01 10:09:48 +0530

    "check_style_score -> 1 of 6 passed. style score: 0/1 "

--- M 6/Assignment-1/Solution.java
+++ M 6/Assignment-1/Solution.java
@@ -27,7 +27,7 @@ public final class Solution {
             if (oddcnt[i] == 1) {
             	int primecnt = 0;
             	for (int j = 1; j < i; j++)
-            	if (i % j == 0) {
+            	if (i % j != 0) {
             		primecnt = primecnt + 1;
             	}
             	if (primecnt == 1) {
