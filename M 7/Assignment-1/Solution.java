7f11d484bdbd098a502031fdbf792125e43f9ada
ASairithwikmahateja <avschalapathirao@msitprogram.net>
2018-09-03 17:34:07 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- M 7/Assignment-1/Solution.java
+++ M 7/Assignment-1/Solution.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 class InputValidator {
     /*Write the atrributes and methods for InputValidator.*/
     /**
-	 * @String  str
-	 */
-	private String str;
+     * @String  str
+     */
+    private String str;
     /**
     @default constructor
     */
@@ -15,40 +15,43 @@ class InputValidator {
      *
      * @param      input  String
      */
-	InputValidator(final String input) {
-	    str = input;
-	}
-	/**
-	 *
-	 * @param      input  String
-	 *
-	 * @return     boolean
-	 */
+    InputValidator(final String input) {
+        str = input;
+    }
+    /**
+     *
+     * @param      input  String
+     *
+     * @return     boolean
+     */
     public boolean validateData(final String input) {
         int len = input.length();
         final int six = 6;
         if (len >= six) {
-        	return true;
+            return true;
         }
         return false;
     }
 }
+/**
+ * Main class
+ */
 public final class Solution {
-	/*
+    /*
     Do not modify the main function
     */
     /**
     @default constructor
     */
-	private Solution() { }
-	/**
+    private Solution() { }
+    /**
      *
      * @param      args    String
      */
-	public static void main(final String[] args) {
-    	Scanner sc = new Scanner(System.in);
-    	String input = sc.nextLine();
-    	InputValidator i = new InputValidator(input);
-    	System.out.println(i.validateData(input));
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        InputValidator i = new InputValidator(input);
+        System.out.println(i.validateData(input));
     }
 }
