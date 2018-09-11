136bd868dd0f129c4c066f0c865114d212b6aac8
ASairithwikmahateja <avschalapathirao@msitprogram.net>
2018-09-11 14:51:04 +0530

    "check_style_score -> 2 of 3 passed. style score: 0/1 "

---
+++ M 15/Assignment-1/Solution.java
@@ -0,0 +1,386 @@
+import java.io.BufferedInputStream;
+import java.util.Scanner;
+import java.util.Arrays;
+/**
+ * Exception for signaling invalid position errors.
+ */
+class InvalidPositionException extends Exception {
+    /**
+     * Constructs the object.
+     * @param      s     String
+     */
+    InvalidPositionException(final String s) {
+        super(s);
+    }
+}
+/**
+ * Exception for signaling index out of bounds errors.
+ */
+class IndexOutOfBoundsException extends Exception {
+    /**
+     * Constructs the object.
+     * @param      s     String.
+     */
+    IndexOutOfBoundsException(final String s) {
+        super(s);
+    }
+}
+/**
+ * List class.
+ */
+public class Solution {
+    /**
+     * int array.
+     */
+    public int[] arr;
+    /**
+     * int size.
+     */
+    public int size;
+    /**
+     * Constructs the object.
+     */
+    public Solution() {
+        final int ten = 10;
+        arr = new int[ten];
+        size = 0;
+    }
+    /**
+     * Constructs the object.
+     * @param      n     int
+     */
+    public Solution(final int n) {
+        size = 0;
+        arr = new int[n];
+    }
+    /**
+     * add method.
+     * @param      item  The item
+     */
+    public void add(final int item) {
+        //Inserts the specified element at the end of the list.
+        if (size == arr.length) {
+            arr = resize();
+        }
+        arr[size++] = item;
+    }
+    /**
+     * resize method.
+     * @return    newarray
+     */
+    public int[] resize() {
+        int n = 2 * size;
+        int[] newarr = java.util.Arrays.copyOf(arr, n);
+        return newarr;
+    }
+    /**
+     * size method.
+     * @return    size
+     */
+    public int size() {
+        // replace the code below to implement the size method
+        return size;
+    }
+    /**
+     * remove method.
+     *
+     * @param      index                     The index
+     *
+     * @throws     InvalidPositionException  
+     */
+    public void remove(final int index) throws InvalidPositionException {
+        if (index >= 0 && index < size) {
+            for (int i = index; i < size - 1; i++) {
+                arr[i] = arr[i + 1];
+            }
+            size--;
+        } else {
+            throw new InvalidPositionException("Invalid Position Exception");
+        }
+    }
+    /**
+     * get method.
+     *
+     * @param      index  The index
+     *
+     * @return     int 
+     */
+    public int get(final int index) {
+        // Replace the code below to write the code for get
+        if (index >= size) {
+            return -1;
+        }
+        return arr[index];
+    }
+
+    /**
+     * Returns a string representation of the object.
+     *
+     * @return     String representation of the object.
+     */
+    public String toString() {
+        // Replace the code below
+        int[] newarr = new int[size];
+        for (int i = 0; i < size; i++) {
+            newarr[i] = arr[i];
+        }
+        return Arrays.toString(newarr).replace(" ", "");
+    }
+    /**
+     * contains method.
+     *
+     * @param      item  The item
+     *
+     * @return     boolean
+     */
+    public boolean contains(final int item) {
+        // Replace the code below
+        for (int i = 0; i < size; i++) {
+            if (arr[i] == item) {
+                return true;
+            }
+        }
+        return false;
+    }
+    /**
+     * Searches for the first match.
+     *
+     * @param      item  The item
+     *
+     * @return     int
+     */
+    public int indexOf(final int item) {
+        // Replace the code below
+        for (int i = 0; i < size; i++) {
+            if (item == arr[i]) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    /**
+     * Adds all.
+     *
+     * @param      newArray  The new array
+     */
+    public void addAll(final int[] newArray) {
+        // write the logic
+        int[] resArray = Arrays.copyOf(newArray, newArray.length);
+        if (size == newArray.length) {
+            resArray = resize();
+        }
+        int newsize = size;
+        for (int i = newsize, j = 0; i < newsize + resArray.length; i++) {
+            add(resArray[j]);
+            j++;
+        }
+    }
+    /**
+     * Removes all.
+     *
+     * @param      newArray                  The new array
+     *
+     * @throws     InvalidPositionException
+     */
+    public void removeAll(final int[] newArray)
+    throws InvalidPositionException {
+        // write the logic
+        for (int i = 0; i < newArray.length; i++) {
+            for (int j = 0; j < size; j++) {
+                if (arr[j] == newArray[i]) {
+                    remove(j);
+                    j--;
+                }
+            }
+        }
+    }
+    /**
+     * subList method.
+     *
+     * @param      start                      The start
+     * @param      end                        The end
+     *
+     * @return     List
+     *
+     * @throws     IndexOutOfBoundsException  { exception_description }
+     */
+    public List subList(final int start, final int end)
+    throws IndexOutOfBoundsException {
+        // write the logic for subList
+        List l = new List();
+        if ((start <= 0 || end <= 0) || (start > end) || size == 0) {
+            throw new IndexOutOfBoundsException(
+                "Index Out of Bounds Exception");
+        } else {
+            for (int i = start; i < end; i++) {
+                l.add(arr[i]);
+            }
+        }
+        return l;
+    }
+    /**
+     * equals method.
+     *
+     * @param      list  The list
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public boolean equals(final List list) {
+        if (size != list.size) {
+            return false;
+        }
+        for (int i = 0; i < size; i++) {
+            if (arr[i] != list.list[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    /**
+     * clear method.
+     */
+    public void clear() {
+        // write the logic for clear.
+        final int defaultsize = 10;
+        arr = new int[defaultsize];
+        size = 0;
+    }
+    /**
+     * count method.
+     *
+     * @param      ele   The ele
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int count(final int ele) {
+        int cnt = 0;
+        for (int i = 0; i < size; i++) {
+            if (arr[i] == ele) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        // create an object of the list to invoke methods on it
+        List l = new List();
+
+        // code to read the test cases input file
+        Scanner stdin = new Scanner(new BufferedInputStream(System.in));
+        // check if there is one more line to process
+        while (stdin.hasNext()) {
+            // read the line
+            String line = stdin.nextLine();
+            // split the line using space
+            String[] tokens = line.split(" ");
+            // based on the list operation invoke the corresponding method
+            switch (tokens[0]) {
+            case "add":
+                if (tokens.length == 2) {
+                    String[] t = tokens[1].split(",");
+                    if (t.length == 1) {
+                        l.add(Integer.parseInt(tokens[1]));
+                    }
+                }
+                break;
+            case "size":
+                System.out.println(l.size());
+                break;
+            case "print":
+                System.out.println(l);
+                break;
+            case "remove":
+                try {
+                    if (tokens.length == 2) {
+                        l.remove(Integer.parseInt(tokens[1]));
+                    }
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+                break;
+            case "indexOf":
+                if (tokens.length == 2) {
+                    System.out.println(l.indexOf(
+                                           Integer.parseInt(tokens[1])));
+                }
+                break;
+            case "get":
+                if (tokens.length == 2) {
+                    System.out.println(l.get(
+                                           Integer.parseInt(tokens[1])));
+                }
+                break;
+            case "contains":
+                if (tokens.length == 2) {
+                    System.out.println(l.contains(
+                                           Integer.parseInt(tokens[1])));
+                }
+                break;
+            case "addAll":
+                if (tokens.length == 2) {
+                    String[] t1 = tokens[1].split(",");
+                    int[] temp = new int[t1.length];
+                    for (int i = 0; i < temp.length; i++) {
+                        temp[i] = Integer.parseInt(t1[i]);
+                    }
+                    l.addAll(temp);
+                }
+                break;
+            case "removeAll":
+                try {
+                    if (tokens.length == 2) {
+                        String[] t2 = tokens[1].split(",");
+                        int[] a = new int[t2.length];
+                        for (int i = 0; i < t2.length; i++) {
+                            a[i] = Integer.parseInt(t2[i]);
+                        }
+                        l.removeAll(a);
+                    }
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+                break;
+            case "subList":
+                try {
+                    if (tokens.length != 2) {
+                        break;
+                    }
+                    String[] arrstring3 = tokens[1].split(",");
+                    List object = l.subList(Integer.parseInt(arrstring3[0]),
+                                            Integer.parseInt(arrstring3[1]));
+                    if (object != null) {
+                        System.out.println(object);
+                    }
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+                break;
+            case "equals":
+                if (tokens.length == 2) {
+                    String[] lt = tokens[1].split(",");
+                    List l2 = new List();
+                    for (int k = 0; k < lt.length; k++) {
+                        l2.add(Integer.parseInt(lt[k]));
+                    }
+                    System.out.println(l.equals(l2));
+                }
+                break;
+            case "clear":
+                l.clear();
+                break;
+            case "count":
+                int res = l.count(Integer.parseInt(tokens[1]));
+                System.out.println(res);
+            default:
+                break;
+            }
+        }
+    }
+}
+
+
